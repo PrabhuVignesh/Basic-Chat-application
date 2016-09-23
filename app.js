e42c2d12b885832a315537a4f9d8ab07f456422e
Prabhu Vignesh <pabhuvignesh.k@hcl.com>
2016-09-23 12:21:25 +0530

    changed to bluemix envi

--- app.js
+++ app.js
@@ -27,8 +27,16 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
   res.render('home.jade');
 });
-server.listen(appPort);
-// app.listen(appPort);
+//server.listen(appPort);
+//app.listen(appPort);
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
 console.log("Server listening on port " + appPort);
 
 // Handle the socket.io connections
